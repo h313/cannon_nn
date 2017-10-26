72d231b742fd65be67c6f7c7554c849b61160da9
veggiedefender <jessejesse123@gmail.com>
2017-10-26 18:09:57 -0400

    pep8

--- test/convert.py
+++ test/convert.py
@@ -4,7 +4,6 @@ import os
 
 
 def csv_from_excel(workbook):
-
     wb = xlrd.open_workbook(workbook)
     sh = wb.sheet_by_name(workbook[:7])
     csv_file = open(workbook[:7] + '.csv', 'wb')
@@ -15,7 +14,6 @@ def csv_from_excel(workbook):
     csv_file.close()
 
 
-import os
 for filename in os.listdir('./'):
 	if filename[-5:] == '.xlsx':
 		csv_from_excel(filename)
