f3adca193034d347e51bd30ad55bde021b3920e5
Harry <harry@h313.info>
2017-10-26 19:16:04 -0400

    do things

--- training/convert.py
+++ training/convert.py
@@ -4,8 +4,8 @@ import os
 
 
 def csv_from_excel(workbook):
-    wb = xlrd.open_workbook(workbook)
-    sh = wb.sheet_by_name(workbook[:7])
+    wb = xlrd.open_workbook('xlsx/' + workbook)
+    sh = wb.sheet_by_name(workbook[:6])
     csv_file = open(workbook[:7] + '.csv', 'wb')
     wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
     for rownum in range(sh.nrows):
