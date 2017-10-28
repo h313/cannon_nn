1c13d96554ed0f6d263bcfd83fb6a72dc9da8b4f
Harry <harry@h313.info>
2017-10-28 17:52:23 -0400

    runnable dnn

--- dnn.py
+++ dnn.py
@@ -6,25 +6,37 @@ Skott20.csv are in training/ and Skott21.csv to Skott25.csv are in
 test/
 """
 from keras.models import Sequential
-from keras.layers import Dense, Activation, LSTM
+from keras.layers import Dense, Activation
 import numpy as np
 import scipy as sp
 
 # Generate training dataset
 data = np.genfromtxt('./training/Skott1.csv', delimiter=',')
 data = np.swapaxes(data, 0, 1)
-train_X = np.array([data[0], data[1], data[3]])
-train_y = data[4]
+train_X = np.swapaxes(np.array([data[0], data[1], data[3]]), 0, 1)
+train_y = np.array(data[4])
 
 # Generate testing dataset
 data1 = np.genfromtxt('./training/Skott2.csv', delimiter=',')
 data1 = np.swapaxes(data1, 0, 1)
-test_X = np.array([data1[0], data1[1], data1[3]])
-test_y = data1[1]
+test_X = np.swapaxes(np.array([data1[0], data1[1], data1[3]]), 0, 1)
+print(test_X)
+test_y = np.array(data1[1])
 
-model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
+model = Sequential([
+    Dense(3, input_shape=(3,)),
+    Activation('relu'),
+    Dense(10),
+    Activation('softmax'),
+    Dense(10),
+    Activation('softmax'),
+    Dense(1),
+    Activation('sigmoid'),
+])
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
 # fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+model.fit(train_X, train_y, epochs=10, batch_size=32)
