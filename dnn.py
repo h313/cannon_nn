8b8e41f2425207232f0ecd8650c929ee953258ac
Haoda Wang <harry@h313.info>
2018-01-06 18:51:21 -0500

    added things

--- dnn.py
+++ dnn.py
@@ -9,8 +9,10 @@ import numpy as np
 
 # Generate dataset
 train_data = np.swapaxes(np.genfromtxt('./data.csv', delimiter=','), 0, 1)
+multipliers = train_data[:, 0]
+train_data = np.delete(train_data, 0, axis=1)
 train_X = np.swapaxes(np.array([train_data[0], train_data[1]]), 0, 1)
-train_y = np.swapaxes(np.array([train_data[2], train_data[3]]), 0, 1)
+train_y = np.swapaxes(np.array([train_data[3], train_data[4]]), 0, 1)
 
 # Define the neural network
 model = Sequential()
@@ -20,9 +22,9 @@ model.add(Dense(units=10, kernel_initializer='zeros', activation='relu'))
 model.add(Dense(units=2, kernel_initializer='zeros'))
 
 # Compile the neural network
-model.compile(optimizer=RMSprop(lr=0.0001), loss='mean_squared_error', metrics=['binary_accuracy'])
+model.compile(optimizer=RMSprop(lr=0.001), loss='mean_squared_error', metrics=['accuracy'])
 
 # Fit the neural network to data
-model.fit(train_X, train_y, epochs=1, batch_size=1, verbose=1, shuffle=True, validation_split=0.01)
+model.fit(train_X, train_y, epochs=3, batch_size=1, verbose=1, shuffle=True, validation_split=0.01)
 
 print(model.predict(np.array([[0.0, 0.0], [780.0, 0.7]]), verbose=1))
