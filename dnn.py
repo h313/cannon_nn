8c5375915c1774584b86b9020fc567ad4be749af
Haoda Wang <harry@h313.info>
2018-01-06 19:25:49 -0500

    get val_loss into 4th decimal place

--- dnn.py
+++ dnn.py
@@ -11,13 +11,13 @@ import numpy as np
 train_data = np.swapaxes(np.genfromtxt('./data.csv', delimiter=','), 0, 1)
 multipliers = train_data[:, 0]
 train_data = np.delete(train_data, 0, axis=1)
-train_X = np.swapaxes(np.array([train_data[0], train_data[1], train_data[2]]), 0, 1)
+train_X = np.swapaxes(np.array([train_data[0], train_data[1]]), 0, 1)
 train_y = np.swapaxes(np.array([train_data[3], train_data[4]]), 0, 1)
 
 # Define the neural network
 model = Sequential()
 
-model.add(Dense(3, input_dim=3, kernel_initializer='zeros', activation='sigmoid'))
+model.add(Dense(2, input_dim=2, kernel_initializer='zeros', activation='sigmoid'))
 model.add(Dense(units=10, kernel_initializer='zeros', activation='sigmoid'))
 model.add(Dense(units=20, kernel_initializer='zeros', activation='sigmoid'))
 model.add(Dense(units=10, kernel_initializer='zeros', activation='sigmoid'))
@@ -29,6 +29,4 @@ model.compile(optimizer=Adam(),
               metrics=['accuracy'])
 
 # Fit the neural network to data
-model.fit(train_X, train_y, epochs=3, batch_size=1, verbose=1, shuffle=True, validation_split=0.001)
-
-print(model.predict(np.array([[0.0, 0.333333333333, 0.98777046096]]), verbose=1))
+model.fit(train_X, train_y, epochs=10, batch_size=1, verbose=1, shuffle=True, validation_split=0.01)
