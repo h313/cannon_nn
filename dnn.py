5311256b2314f036e9227448545c09e95fc4c33b
Harry <harry@h313.info>
2018-01-09 09:03:24 -0500

    switch to xy instead

--- dnn.py
+++ dnn.py
@@ -19,7 +19,7 @@ multipliers = train_data[:, 0]
 train_data = np.delete(train_data, 0, axis=1)
 # Convert the training data into two arrays, one of input data and one of output data
 train_X = np.swapaxes(np.array([train_data[0], train_data[1]]), 0, 1)
-train_y = np.swapaxes(np.array([train_data[3], train_data[4]]), 0, 1)
+train_y = np.swapaxes(np.array([train_data[2], train_data[3], train_data[4]]), 0, 1)
 
 # Define the neural network
 model = Sequential()
@@ -32,7 +32,7 @@ model.add(Dense(2, input_dim=2, kernel_initializer='zeros', activation='sigmoid'
 model.add(Dense(units=10, kernel_initializer='zeros', activation='sigmoid'))
 model.add(Dense(units=20, kernel_initializer='zeros', activation='sigmoid'))
 model.add(Dense(units=10, kernel_initializer='zeros', activation='sigmoid'))
-model.add(Dense(units=2, kernel_initializer='zeros'))
+model.add(Dense(units=3, kernel_initializer='zeros'))
 
 # Compile the neural network
 '''
